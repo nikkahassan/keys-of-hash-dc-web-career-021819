ed7e004dc3e281f8e03f98d73b1271044d7b1904
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-15 20:07:19 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,1 +1,16 @@
 class Hash
+def keys_of(*arguments)
+    array = []
+    self.each do |key, value|
+      arguments.each do |i|
+        if i == value
+        array.push(key)
+        end
+      end 
+
+
+    end
+    return array
+  end
+  
+end 
